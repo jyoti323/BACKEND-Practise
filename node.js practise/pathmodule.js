4a0b82aa6a47b4bc9743ee8a3fd33c3549917db5
Jyoti sharma <jyotisharma4213@gmail.com>
2026-06-25 10:27:04 +0530

    Add pathmodule.js and update osmodule.js

---
+++ node.js practise/pathmodule.js
@@ -0,0 +1,10 @@
+const path=require('path')
+
+const a=path.basename('C:\\temp\\myfile.html')
+const b=path.dirname('C:\\temp\\myfile.html')
+console.log(a)
+console.log(b)
+
+const c=path.extname(__filename)
+console.log(c)
+console.log(__filename)
